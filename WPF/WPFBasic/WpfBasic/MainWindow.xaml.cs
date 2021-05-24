de48b3da74606091c36847c60523ed72836f2f25
unknown <mshabazi68@gmail.com>
2021-05-24 02:23:45 -0700

    Adding the apply and reset btn

--- WPF/WPFBasic/WpfBasic/MainWindow.xaml.cs
+++ WPF/WPFBasic/WpfBasic/MainWindow.xaml.cs
@@ -24,5 +24,39 @@ namespace WpfBasic
         {
             InitializeComponent();
         }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"You submit the following text: { this.DescriptionText.Text}");
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.welldCheck.IsChecked = this.AssemblyCheck.IsChecked = this.PlasmaCheck.IsChecked = this.LaserCheck.IsChecked = this.PlasmaCheck.IsChecked = this.latheCheck.IsChecked = this.PurchaseCheck.IsChecked= this.DrillCheck.IsChecked = this.FoldCheck.IsChecked = this.RollCheck.IsChecked = this.SawCheck.IsChecked = false;
+
+            this.LengthText.Text = String.Empty;
+        }
+
+        private void welldCheck_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Check_Checked(object sender, RoutedEventArgs e)
+        {
+            this.LengthText.Text += ((CheckBox)sender).Content;
+
+        }
+
+        private void FinishDropdown_selection(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.NoteText == null)
+            {
+                return;
+            }
+            var combo = (ComboBox)sender;
+            var value = (ComboBoxItem)combo.SelectedValue;
+            this.NoteText.Text = (string) value.Content ;
+        }
     }
 }
